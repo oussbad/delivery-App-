602ee4945edc6bef8b894d7e21ba6af86bda8c37
TheSerboutiAbdelaziz <serbouti.abdelaziz@gmail.com>
2023-06-07 14:45:07 +0100

    feat: installing & setting up device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    DevicePreview(
+      enabled: true,
+      isToolbarVisible: false,
+      defaultDevice: DevicePreview.defaultDevices.firstWhere(
+        (device) => device.name == "Small",
+      ),
+      backgroundColor: Colors.black87,
+      tools: const [
+        ...DevicePreview.defaultTools,
+      ],
+      builder: (_) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
