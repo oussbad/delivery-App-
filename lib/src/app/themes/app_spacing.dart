a609385cd2ddc929e7365acf99c5086743a3383e
TheSerboutiAbdelaziz <serbouti.abdelaziz@gmail.com>
2023-06-08 13:16:26 +0100

    feat: add app spacing

--- lib/src/app/themes/app_spacing.dart
+++ lib/src/app/themes/app_spacing.dart
@@ -1,3 +1,26 @@
+import 'package:flutter/material.dart';
+
 class AppSpacing {
   AppSpacing._();
+
+  static double h_4 = 4.0;
+  static double h_8 = 8.0;
+  static double h_12 = 12.0;
+  static double h_16 = 16.0;
+  static double h_20 = 20.0;
+  static double h_24 = 24.0;
+  static double h_28 = 28.0;
+  static double h_32 = 32.0;
+  static double h_36 = 36.0;
+  static double h_40 = 40.0;
+  static double h_44 = 44.0;
+  static double h_48 = 48.0;
+  static double h_52 = 42.0;
+  static double h_56 = 56.0;
+  static double h_60 = 60.0;
+  static double h_64 = 64.0;
+
+  static double r_10 = 10.0;
+
+  static Size mediaQuery(BuildContext context) => MediaQuery.of(context).size;
 }
