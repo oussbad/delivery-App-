8ea6ef795a3655866352cc6d4b2ef9d48fa156e5
TheSerboutiAbdelaziz <serbouti.abdelaziz@gmail.com>
2023-06-09 00:32:28 +0100

    feat: create radio widget

---
+++ lib/src/app/widgets/inputs/radio_widget.dart
@@ -0,0 +1,39 @@
+import 'package:boxpend_flutter_android_app/src/app/themes/app_palette.dart';
+import 'package:boxpend_flutter_android_app/src/app/widgets/utils/gap_widget.dart';
+import 'package:boxpend_flutter_android_app/src/app/widgets/utils/text_block_widget.dart';
+import 'package:flutter/material.dart';
+
+class RadioWidget<T> extends StatelessWidget {
+  const RadioWidget({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.groupValue,
+    required this.onChanged,
+  });
+
+  final String label;
+  final T value;
+  final T groupValue;
+  final void Function(T?) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Radio<T>(
+          value: value,
+          groupValue: groupValue,
+          onChanged: onChanged,
+        ),
+        GapWidget.spacing8(
+          axe: GapEnum.horizontal,
+        ),
+        TextBlocKWidget.body(
+          label,
+          color: AppPalette.white,
+        ),
+      ],
+    );
+  }
+}
