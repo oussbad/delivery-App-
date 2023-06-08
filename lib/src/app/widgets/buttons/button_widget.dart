45909a7f45a66b88625f5b1ee742514ad890e7ec
TheSerboutiAbdelaziz <serbouti.abdelaziz@gmail.com>
2023-06-08 16:49:35 +0100

    feat: create button widget

---
+++ lib/src/app/widgets/buttons/button_widget.dart
@@ -0,0 +1,32 @@
+import 'package:boxpend_flutter_android_app/src/app/themes/app_spacing.dart';
+import 'package:boxpend_flutter_android_app/src/app/widgets/utils/text_block_widget.dart';
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget({
+    super.key,
+    required this.label,
+    required this.onPressed,
+    this.backgroundColor,
+  });
+
+  final String label;
+  final VoidCallback onPressed;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(backgroundColor),
+      ),
+      child: Container(
+        width: double.infinity,
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(vertical: AppSpacing.s_16),
+        child: TextBlocKWidget.body(label),
+      ),
+    );
+  }
+}
--- lib/src/presentation/template/pages/template_page.dart
+++ lib/src/presentation/template/pages/template_page.dart
@@ -1,4 +1,6 @@
 import 'package:boxpend_flutter_android_app/src/app/resources/strings_manager.dart';
+import 'package:boxpend_flutter_android_app/src/app/themes/app_palette.dart';
+import 'package:boxpend_flutter_android_app/src/app/widgets/buttons/button_widget.dart';
 import 'package:boxpend_flutter_android_app/src/app/widgets/utils/gap_widget.dart';
 import 'package:boxpend_flutter_android_app/src/app/widgets/utils/text_block_widget.dart';
 import 'package:boxpend_flutter_android_app/src/presentation/template/controllers/template_controller.dart';
@@ -15,23 +17,41 @@ class Template extends GetView<TemplateController> {
         title: Text(StringsManager.app.tr),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextBlocKWidget.h1('Text H1'),
-            GapWidget.spacing12(),
-            TextBlocKWidget.h2('Text H2'),
-            GapWidget.spacing12(),
-            TextBlocKWidget.h3('Text H3'),
-            GapWidget.spacing12(),
-            TextBlocKWidget.title('Text title'),
-            GapWidget.spacing12(),
-            TextBlocKWidget.subTitlte('Text sub title'),
-            GapWidget.spacing12(),
-            TextBlocKWidget.body('Text body'),
-            GapWidget.spacing12(),
-            TextBlocKWidget.caption('Text caption'),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ButtonWidget(
+                label: 'Submit',
+                onPressed: () {},
+              ),
+              GapWidget.spacing12(),
+              ButtonWidget(
+                label: 'Submit',
+                backgroundColor: AppPalette.danger,
+                onPressed: () {},
+              ),
+              GapWidget.spacing12(),
+              ButtonWidget(
+                label: 'Submit',
+                backgroundColor: AppPalette.success,
+                onPressed: () {},
+              ),
+              GapWidget.spacing12(),
+              ButtonWidget(
+                label: 'Submit',
+                backgroundColor: AppPalette.warning,
+                onPressed: () {},
+              ),
+              GapWidget.spacing12(),
+              ButtonWidget(
+                label: 'Submit',
+                backgroundColor: AppPalette.info,
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
