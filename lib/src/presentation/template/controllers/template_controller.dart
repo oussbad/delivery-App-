1256ed8be24cd3a67c5071f325062b98819204da
omar <el.ell.elassani@gmail.com>
2023-07-06 22:50:17 +0100

    i create Network abstract class and his implementation

--- lib/src/presentation/template/controllers/template_controller.dart
+++ lib/src/presentation/template/controllers/template_controller.dart
@@ -1,5 +1,15 @@
 import 'package:get/get.dart';
 
+import '../../../app/services/network_service.dart';
+
 class TemplateController extends GetxController {
-  static TemplateController get to => Get.find();
+  static TemplateController get to => Get.find(); 
+  NetworkService networkService = NetworkService();
+  @override
+  void onInit() {
+    // check if internet is connected or not 
+    networkService.isConnected().then((value) => print(value));  
+    super.onInit();
+  }
+  
 }
