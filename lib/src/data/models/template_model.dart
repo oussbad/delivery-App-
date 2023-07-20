f841b185432418192a60769b6279f56faacaecf4
TheSerboutiAbdelaziz <serbouti.abdelaziz@gmail.com>
2023-07-20 13:33:29 +0100

    feat: add template model

---
+++ lib/src/data/models/template_model.dart
@@ -0,0 +1,18 @@
+import '../../domain/template/entities/template_entitiy.dart';
+
+class TemplateModel extends Template {
+  const TemplateModel({
+    required super.id,
+    required super.name,
+  });
+
+  factory TemplateModel.fromJson(Map<String, dynamic> json) => TemplateModel(
+        id: json["id"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+      };
+}
