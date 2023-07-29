ec57cfac184cc20f2677d1e2d667675fc5be8d76
oussbad <oussamabadreddine210@gmail.com>
2023-07-29 14:39:11 +0100

    feat & fix : add light theme & update sittings bind and sittings controller

--- lib/src/app/themes/app_themes.dart
+++ lib/src/app/themes/app_themes.dart
@@ -58,4 +58,58 @@ class AppThemes {
       type: BottomNavigationBarType.fixed,
     ),
   );
+
+  // Light theme
+  static ThemeData light = ThemeData(
+    //fontFamily: AppTypography.familyInter,
+    primarySwatch: AppPalette.primarySwatch,
+    scaffoldBackgroundColor: Colors.white, // Set the background color for light theme
+    hintColor: AppPalette.inputBorder,
+    textTheme: TextTheme(
+      bodyMedium: TextStyle(
+        color: Colors.black, // Set the text color for light theme
+      ),
+      bodyLarge: TextStyle(
+        color: Colors.black, // Set the text color for light theme
+      ),
+      bodySmall: TextStyle(
+        color: Colors.black, // Set the text color for light theme
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: AppPalette.inputBg,
+      border: OutlineInputBorder(
+        borderSide: BorderSide(color: AppPalette.inputBorder),
+      ),
+    ),
+    checkboxTheme: CheckboxThemeData(
+      fillColor: MaterialStateProperty.all<Color>(
+        AppPalette.primarySwatch,
+      ),
+      side: BorderSide(
+        color: AppPalette.inputBorder,
+      ),
+    ),
+    radioTheme: RadioThemeData(
+      fillColor: MaterialStateProperty.all<Color>(
+        AppPalette.primarySwatch,
+      ),
+    ),
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppPalette.inputBorder,
+    ),
+    drawerTheme: DrawerThemeData(
+      backgroundColor: AppPalette.bg4,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      elevation: 2,
+      selectedItemColor: Colors.black, // Set the selected item color for light theme
+      unselectedItemColor: AppPalette.primarySwatch,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      backgroundColor: AppPalette.inputBorder,
+      type: BottomNavigationBarType.fixed,
+    ),
+  );
 }
