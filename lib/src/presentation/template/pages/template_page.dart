346269ce71c3410877d4b2d123b7dfbb73800778
omar <el.ell.elassani@gmail.com>
2023-07-30 10:40:02 +0100

    creating AppSnackBar extention and testing it in the tamplate file

--- lib/src/presentation/template/pages/template_page.dart
+++ lib/src/presentation/template/pages/template_page.dart
@@ -1,7 +1,10 @@
+import 'package:boxpend_flutter_android_app/src/app/helpers/app_snackbar.dart';
 import 'package:boxpend_flutter_android_app/src/app/themes/app_spacing.dart';
 import 'package:boxpend_flutter_android_app/src/presentation/template/controllers/template_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../../app/extensions/app_snackbar.dart';
 
 class Template extends GetView<TemplateController> {
   const Template({Key? key}) : super(key: key);
@@ -14,8 +17,32 @@ class Template extends GetView<TemplateController> {
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text('Template'),
+            children: [
+              const Text('Template'),
+              ElevatedButton(
+                onPressed: () {
+                  AppSnackbarExtension.showAppSnackbar(
+                    title: "done ",
+                    message: " your action is done ",
+                    icon: Icons.check,
+                  );
+                  // AppSnackbarExtension.showAppSnackbarWithAction(
+                  //   title: "done ",
+                  //   message: " your action is done ",
+                  //   icon: Icons.check,
+                  //   actionTitle: "undo",
+                  //   actionFunction: () {
+                  //     AppSnackbarExtension.showAppSnackbar(
+                  //       title: "undo",
+                  //       message: " your action is undone ",
+                  //       icon: Icons.undo,
+                  //     );
+                  //   },
+                  // );
+
+                },
+                child: const Text("open snackbar"),
+              ),
             ],
           ),
         ),
