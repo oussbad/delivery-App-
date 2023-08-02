49a2fdefe49b18831f4890e914962b3b39bb05ab
oussbad <oussamabadreddine210@gmail.com>
2023-08-02 13:37:03 +0100

    fix:  create tow radio buttons one for light mode and another for dark mode in settings page

--- lib/src/presentation/settings/pages/settings_page.dart
+++ lib/src/presentation/settings/pages/settings_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../controllers/settings_controller.dart'; 
-class SettingsPage extends GetView<SettingsController>{
+import '../../../app/themes/app_themes.dart';
+import '../controllers/settings_controller.dart';
+
+class SettingsPage extends GetView<SettingsController> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-
-    throw UnimplementedError();
+    final SettingsController settingsController = Get.find();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RadioListTile(
+                value: AppThemes.light,
+                groupValue: settingsController,
+                onChanged: (value) => {}),
+            RadioListTile(
+                value: AppThemes.light,
+                groupValue: settingsController,
+                onChanged: (value) => {})
+          ],
+        ),
+      ),
+    );
   }
-  
 }
