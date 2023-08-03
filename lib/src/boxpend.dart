172e910f461ee68063979a1803f100ad3387c9b2
oussbad <oussamabadreddine210@gmail.com>
2023-08-03 12:09:03 +0100

    fix: add dark theme and theme mode in Boxpand.dart file and update settings controller.dart

--- lib/src/boxpend.dart
+++ lib/src/boxpend.dart
@@ -1,6 +1,7 @@
 import 'package:boxpend_flutter_android_app/src/app/localization/app_localization.dart';
 import 'package:boxpend_flutter_android_app/src/app/routes/app_pages.dart';
 import 'package:boxpend_flutter_android_app/src/app/themes/app_themes.dart';
+import 'package:boxpend_flutter_android_app/src/presentation/settings/controllers/settings_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -16,7 +17,9 @@ class Boxpend extends StatelessWidget {
       getPages: AppPages.pages,
       locale: AppLocalization.defaultLanguage,
       translations: AppLocalization.getInstance(),
-      theme: AppThemes.dark,
+      theme: AppThemes.light,
+      darkTheme: AppThemes.dark,
+      themeMode: SettingsController().getThemeMode(),
     );
   }
 }
--- lib/src/presentation/settings/controllers/settings_controller.dart
+++ lib/src/presentation/settings/controllers/settings_controller.dart
@@ -1,7 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SettingsController extends GetxController{
+import '../../../app/services/local_storage_service_impl.dart';
+import '../../../app/themes/app_themes.dart';
+
+class SettingsController extends GetxController {
   static SettingsController get to => Get.find();
+  final _darkThemeKey = 'isDarkTheme';
+  LocalStorageServiceImpl localStorageService = LocalStorageServiceImpl();
 
+  Future<void> SaveThemeData(bool isDarkMode) async {
+    localStorageService.save(_darkThemeKey, isDarkMode);
+  }
 
+  bool isSavedDarkMode() {
+    return localStorageService.get(_darkThemeKey) ?? false;
+  }
+
+  ThemeMode getThemeMode(){
+    return isSavedDarkMode() ? ThemeMode.dark : ThemeMode.light;
+  }
+  void changeTheme(){
+    Get.changeThemeMode(isSavedDarkMode() ? ThemeMode.light:ThemeMode.dark);
+    SaveThemeData(!isSavedDarkMode());
+  }
 }
