8decc1f2a0f9675e89603d4608fff536bd5a0e5e
Abdallah_Hammad <108339831+AbdallahHammad22@users.noreply.github.com>
2023-08-09 01:47:21 +0200

    update/ Launcher Icon - App Name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:boxpend_flutter_android_app/src/boxpend.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-
 import 'src/app/utils/dependency_injection.dart';
 
 void main() async {
