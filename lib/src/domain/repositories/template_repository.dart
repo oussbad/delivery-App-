fb921bae237ce5db84e9e3bd88a39008d2d5b09a
TheSerboutiAbdelaziz <serbouti.abdelaziz@gmail.com>
2023-08-15 22:02:38 +0100

    fix: correct Entity keyword

--- lib/src/domain/repositories/template_repository.dart
+++ lib/src/domain/repositories/template_repository.dart
@@ -1,5 +1,5 @@
 import 'package:boxpend_flutter_android_app/src/app/core/error/failures.dart';
-import 'package:boxpend_flutter_android_app/src/domain/entities/template_entitiy.dart';
+import 'package:boxpend_flutter_android_app/src/domain/entities/template_entity.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class TemplateRepository {
