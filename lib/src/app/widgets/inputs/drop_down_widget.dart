3d2e4e09d26296ec7394cf47dfe77771e261864d
oussbad <oussamabadreddine210@gmail.com>
2023-09-22 09:02:03 +0100

    feat: creat the drop down widget file dart

---
+++ lib/src/app/widgets/inputs/drop_down_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class DropwdownWidget extends StatelessWidget {
+  const DropwdownWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
