4e83cd99f091fd873fd9b352b9a15708116991b3
TheSerboutiAbdelaziz <serbouti.abdelaziz@gmail.com>
2023-11-14 12:49:50 +0100

    feat: add avatar widget

---
+++ lib/src/app/widgets/avatar_widget.dart
@@ -0,0 +1,109 @@
+import 'package:boxpend_flutter_android_app/src/app/themes/app_palette.dart';
+import 'package:boxpend_flutter_android_app/src/app/themes/app_spacing.dart';
+import 'package:boxpend_flutter_android_app/src/app/widgets/text_block_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+enum AvatarSize { small, medium, large }
+
+class _AvaarBasicWidget extends StatelessWidget {
+  const _AvaarBasicWidget({
+    super.key,
+    required this.asset,
+    required this.size,
+    this.isSuccess,
+  });
+
+  final String asset;
+  final AvatarSize size;
+  final bool? isSuccess;
+
+  @override
+  Widget build(BuildContext context) {
+    return GetUtils.isNull(isSuccess) ? _buildAvatar() : _buildAvatarWithStatus();
+  }
+
+  double _setAvatarSize() {
+    switch (size) {
+      case AvatarSize.small:
+        return AppSpacing.s24;
+      case AvatarSize.medium:
+        return AppSpacing.s36;
+      case AvatarSize.large:
+        return AppSpacing.s44;
+      default:
+        return AppSpacing.s24;
+    }
+  }
+
+  Widget _buildAvatarWithStatus() {
+    return Stack(
+      children: [
+        _buildAvatar(),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: CircleAvatar(
+            backgroundColor: isSuccess! ? AppPalette.success : AppPalette.danger,
+            radius: 10,
+            child: Icon(
+              isSuccess! ? Icons.check_rounded : Icons.close_rounded,
+              color: AppPalette.white,
+              size: AppSpacing.s8,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAvatar() {
+    if (GetUtils.isNullOrBlank(asset)!) {
+      return _buildTextAvatar('?');
+    } else {
+      if (GetUtils.isURL(asset)) {
+        return _buildImageAvatar(asset);
+      } else {
+        return _buildTextAvatar(
+          GetUtils.capitalizeFirst(asset)![0],
+        );
+      }
+    }
+  }
+
+  Widget _buildImageAvatar(String asset) {
+    return CircleAvatar(
+      radius: _setAvatarSize(),
+      backgroundImage: NetworkImage(asset),
+    );
+  }
+
+  Widget _buildTextAvatar(String text) {
+    return CircleAvatar(
+      radius: _setAvatarSize(),
+      backgroundColor: Color(text.hashCode & 0xFFFFFF).withOpacity(1.0),
+      child: Center(
+        child: TextBlocKWidget.t16(
+          text,
+          color: AppPalette.white,
+        ),
+      ),
+    );
+  }
+}
+
+class AvatarWidget extends _AvaarBasicWidget {
+  const AvatarWidget({
+    super.key,
+    required super.asset,
+    super.size = AvatarSize.medium,
+  });
+
+  const AvatarWidget.widthStatus({
+    super.key,
+    required super.asset,
+    required super.isSuccess,
+  }) : super(
+          size: AvatarSize.small,
+        );
+}
--- lib/src/presentation/template/pages/test_page.dart
+++ lib/src/presentation/template/pages/test_page.dart
@@ -7,7 +7,7 @@ import 'package:boxpend_flutter_android_app/src/app/widgets/drop_down_widget.dar
 import 'package:boxpend_flutter_android_app/src/app/widgets/gap_widget.dart';
 import 'package:boxpend_flutter_android_app/src/app/widgets/icon_button_widget.dart';
 import 'package:boxpend_flutter_android_app/src/app/widgets/radio_widget.dart';
-import 'package:boxpend_flutter_android_app/src/app/widgets/state_widget.dart';
+import 'package:boxpend_flutter_android_app/src/app/widgets/avatar_widget.dart';
 import 'package:boxpend_flutter_android_app/src/app/widgets/text_field_widget.dart';
 import 'package:boxpend_flutter_android_app/src/presentation/template/controllers/test_controller.dart';
 import 'package:flutter/material.dart';
@@ -25,7 +25,25 @@ class TestPage extends GetView<TestController> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              AvatarWidget.widthStatus(
+                isSuccess: true,
+                asset: 'https://i.pravatar.cc/300',
+              ),
               GapWidget.s16(),
+              AvatarWidget.widthStatus(
+                isSuccess: false,
+                asset: 'A',
+              ),
+              GapWidget.s16(),
+              AvatarWidget(
+                size: AvatarSize.medium,
+                asset: 'A',
+              ),
+              GapWidget.s16(),
+              AvatarWidget(
+                size: AvatarSize.large,
+                asset: 'https://i.pravatar.cc/300',
+              ),
             ],
           ),
         ),
